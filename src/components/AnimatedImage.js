a227789f6654c9d56e874180ed82d9caf2319b3c
Khaled Maatouk <khaledmaatouk@hotmail.com>
2024-06-21 19:10:05 +0300

    Second coomit

--- src/components/AnimatedImage.js
+++ src/components/AnimatedImage.js
@@ -6,7 +6,7 @@ import './AnimatedImage.css'; // Import the CSS file
 const AnimatedImage = ({ src, alt }) => {
   return (
     <div className="video-container">
-      <video className="responsive-video" autoPlay loop muted>
+      <video className="responsive-video" autoPlay loop muted playsInline>
         <source src={src} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
